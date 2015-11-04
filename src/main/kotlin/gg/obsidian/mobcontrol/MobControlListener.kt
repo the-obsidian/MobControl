d3cade49113a3fa98dc7ad1902cbc4909e365f0d
Jacob Gillespie <jacobwgillespie@gmail.com>
2015-11-04 09:59:56 -0600

    Reformat code

--- src/main/kotlin/gg/obsidian/mobcontrol/MobControlListener.kt
+++ src/main/kotlin/gg/obsidian/mobcontrol/MobControlListener.kt
@@ -13,46 +13,46 @@ import org.bukkit.inventory.ItemStack
 
 class MobControlListener(private val plugin: MobControl) : Listener {
 
-  @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-  fun onChunkUnload(e: ChunkUnloadEvent) {
-    plugin.mobs.removeFromChunk(e.chunk)
-  }
-
-  @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-  fun onCreatureSpawnEvent(event: CreatureSpawnEvent) {
-    plugin.mobs.limitCreatureSpawn(event)
-  }
-
-  @EventHandler(priority = EventPriority.MONITOR)
-  fun onEntityDeath(event: EntityDeathEvent) {
-    if (plugin.config.BUFF_DROPS <= 1 || event.entity !is Ageable) {
-      return
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    fun onChunkUnload(e: ChunkUnloadEvent) {
+        plugin.mobs.removeFromChunk(e.chunk)
     }
 
-    val items = event.drops
-    val l = event.entity.location
-
-    for (a in items) {
-      if (plugin.config.BUFF_DISABLED_ITEMS.contains(a.typeId)) {
-        continue
-      }
-
-      for (i in 1..plugin.config.BUFF_DROPS - 1) {
-        l.world.dropItemNaturally(l, a)
-      }
-    }
-  }
-
-  @EventHandler(priority = EventPriority.MONITOR)
-  fun onPlayerShearEvent(event: EntityDeathEvent) {
-    if (plugin.config.BUFF_SHEAR_DROPS <= 1 || event.entity !is Sheep) {
-      return
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    fun onCreatureSpawnEvent(event: CreatureSpawnEvent) {
+        plugin.mobs.limitCreatureSpawn(event)
     }
 
-    val entity = event.entity as Sheep
-    val l = entity.location
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onEntityDeath(event: EntityDeathEvent) {
+        if (plugin.config.BUFF_DROPS <= 1 || event.entity !is Ageable) {
+            return
+        }
 
-    val count = (1 + (3 * Math.random()).toInt()) * (plugin.config.BUFF_SHEAR_DROPS - 1)
-    l.world.dropItemNaturally(l, ItemStack(Material.WOOL, count, entity.color.ordinal.toByte().toShort()))
-  }
+        val items = event.drops
+        val l = event.entity.location
+
+        for (a in items) {
+            if (plugin.config.BUFF_DISABLED_ITEMS.contains(a.typeId)) {
+                continue
+            }
+
+            for (i in 1..plugin.config.BUFF_DROPS - 1) {
+                l.world.dropItemNaturally(l, a)
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onPlayerShearEvent(event: EntityDeathEvent) {
+        if (plugin.config.BUFF_SHEAR_DROPS <= 1 || event.entity !is Sheep) {
+            return
+        }
+
+        val entity = event.entity as Sheep
+        val l = entity.location
+
+        val count = (1 + (3 * Math.random()).toInt()) * (plugin.config.BUFF_SHEAR_DROPS - 1)
+        l.world.dropItemNaturally(l, ItemStack(Material.WOOL, count, entity.color.ordinal.toByte().toShort()))
+    }
 }
--- src/main/kotlin/gg/obsidian/mobcontrol/MobManager.kt
+++ src/main/kotlin/gg/obsidian/mobcontrol/MobManager.kt
@@ -9,162 +9,162 @@ import java.util.HashMap
 
 class MobManager(val plugin: MobControl) {
 
-  fun hasCap(entity: Entity): Boolean {
-    return getCap(getCapKey(entity)) >= 0
-  }
-
-  fun getCap(key: String): Int {
-    val cap = plugin.config.MOB_LIMITS[key]
-    return if ((cap != null)) cap else -1
-  }
-
-  fun getCapKey(entity: Entity): String {
-    val key = StringBuilder(entity.type.name)
-
-    if (entity is Sheep) {
-      key.append(entity.color.name)
+    fun hasCap(entity: Entity): Boolean {
+        return getCap(getCapKey(entity)) >= 0
     }
 
-    return key.toString().toLowerCase()
-  }
-
-  fun getMobDescription(entity: Entity): String {
-    val key = getCapKey(entity)
-    val loc = entity.location
-    return "%s at (%s,%d,%d,%d)".format(key, loc.world.name, loc.blockX, loc.blockY, loc.blockZ)
-  }
-
-  fun isSpecialMob(entity: LivingEntity): Boolean {
-    if (entity.customName != null) {
-      return true
+    fun getCap(key: String): Int {
+        val cap = plugin.config.MOB_LIMITS[key]
+        return if ((cap != null)) cap else -1
     }
 
-    if (entity is Tameable) {
-      if (entity.isTamed) {
-        return true
-      }
+    fun getCapKey(entity: Entity): String {
+        val key = StringBuilder(entity.type.name)
+
+        if (entity is Sheep) {
+            key.append(entity.color.name)
+        }
+
+        return key.toString().toLowerCase()
     }
 
-    if (entity.type == EntityType.GUARDIAN && (entity as Guardian).isElder) {
-      return true
+    fun getMobDescription(entity: Entity): String {
+        val key = getCapKey(entity)
+        val loc = entity.location
+        return "%s at (%s,%d,%d,%d)".format(key, loc.world.name, loc.blockX, loc.blockY, loc.blockZ)
     }
 
-    val equipment = entity.equipment
-    for (armor in equipment.armorContents) {
-      if (armor != null && armor.type != Material.AIR) {
-        return true
-      }
+    fun isSpecialMob(entity: LivingEntity): Boolean {
+        if (entity.customName != null) {
+            return true
+        }
+
+        if (entity is Tameable) {
+            if (entity.isTamed) {
+                return true
+            }
+        }
+
+        if (entity.type == EntityType.GUARDIAN && (entity as Guardian).isElder) {
+            return true
+        }
+
+        val equipment = entity.equipment
+        for (armor in equipment.armorContents) {
+            if (armor != null && armor.type != Material.AIR) {
+                return true
+            }
+        }
+
+        return false
     }
 
-    return false
-  }
+    fun removeFromChunk(chunk: Chunk) {
+        val count = HashMap<String, Int>()
 
-  fun removeFromChunk(chunk: Chunk) {
-    val count = HashMap<String, Int>()
+        for (entity in chunk.entities) {
+            if (entity.isDead || !(entity is Animals || entity is Monster)) {
+                continue
+            }
 
-    for (entity in chunk.entities) {
-      if (entity.isDead || !(entity is Animals || entity is Monster)) {
-        continue
-      }
+            // Do not remove special mobs
+            val special = isSpecialMob(entity as LivingEntity)
+            if (special) {
+                if (plugin.config.DEBUG) {
+                    plugin.logger.info("Special mob exempted from removal: " + getMobDescription(entity))
+                }
+                continue
+            }
 
-      // Do not remove special mobs
-      val special = isSpecialMob(entity as LivingEntity)
-      if (special) {
+            val key = getCapKey(entity)
+
+            val oldCount = count[key]
+            val mobCount = if ((oldCount == null)) 1 else oldCount + 1
+            count.put(key, mobCount)
+
+            val cap = getCap(key)
+            if (cap >= 0 && mobCount > cap) {
+                entity.remove()
+                if (plugin.config.DEBUG) {
+                    plugin.logger.info("Removing mob " + getMobDescription(entity))
+                }
+            }
+        }
+    }
+
+    fun removeFromAllChunks() {
+        for (c in plugin.server.worlds[0].loadedChunks) {
+            removeFromChunk(c)
+        }
+    }
+
+    fun applyAgeCap(entity: Animals) {
+        if (entity.ageLock == true) {
+            if (plugin.config.DEBUG) {
+                plugin.logger.info("Age locked " + getMobDescription(entity) + " with age " + entity.age)
+            }
+            return
+        }
+
+        if (plugin.config.AGE_CAP_BABY >= 0 && !entity.isAdult) {
+            entity.age = Math.max(entity.age, -plugin.config.AGE_CAP_BABY)
+        } else if (plugin.config.AGE_CAP_BREED >= 0 && entity.isAdult) {
+            entity.age = Math.min(entity.age, plugin.config.AGE_CAP_BREED)
+        }
+
         if (plugin.config.DEBUG) {
-          plugin.logger.info("Special mob exempted from removal: " + getMobDescription(entity))
+            plugin.logger.info("Age of " + getMobDescription(entity) + " capped to " + entity.age)
         }
-        continue
-      }
+    }
 
-      val key = getCapKey(entity)
+    fun limitCreatureSpawn(event: CreatureSpawnEvent) {
+        val reason = event.spawnReason
 
-      val oldCount = count[key]
-      val mobCount = if ((oldCount == null)) 1 else oldCount + 1
-      count.put(key, mobCount)
+        if ((reason == CreatureSpawnEvent.SpawnReason.BREEDING || reason == CreatureSpawnEvent.SpawnReason.EGG || reason == CreatureSpawnEvent.SpawnReason.DISPENSE_EGG) && isFarmAnimal(event.entity)) {
 
-      val cap = getCap(key)
-      if (cap >= 0 && mobCount > cap) {
-        entity.remove()
-        if (plugin.config.DEBUG) {
-          plugin.logger.info("Removing mob " + getMobDescription(entity))
+            applyAgeCap(event.entity as Animals)
+
+            for (entity in event.entity.getNearbyEntities(4.0, 4.0, 4.0)) {
+                if (isFarmAnimal(entity)) {
+                    applyAgeCap(entity as Animals)
+                }
+            }
+
+            return
         }
-      }
-    }
-  }
 
-  fun removeFromAllChunks() {
-    for (c in plugin.server.worlds[0].loadedChunks) {
-      removeFromChunk(c)
-    }
-  }
+        val shouldLimitNaturalSpawn = plugin.config.LIMIT_NATURAL_SPAWN && (event.spawnReason == CreatureSpawnEvent.SpawnReason.BREEDING || event.spawnReason == CreatureSpawnEvent.SpawnReason.NATURAL || event.spawnReason == CreatureSpawnEvent.SpawnReason.DEFAULT)
+        val shouldLimitSpawnerSpawn = plugin.config.LIMIT_SPAWNER_SPAWN && event.spawnReason == CreatureSpawnEvent.SpawnReason.SPAWNER
 
-  fun applyAgeCap(entity: Animals) {
-    if (entity.ageLock == true) {
-      if (plugin.config.DEBUG) {
-        plugin.logger.info("Age locked " + getMobDescription(entity) + " with age " + entity.age)
-      }
-      return
-    }
-
-    if (plugin.config.AGE_CAP_BABY >= 0 && !entity.isAdult) {
-      entity.age = Math.max(entity.age, -plugin.config.AGE_CAP_BABY)
-    } else if (plugin.config.AGE_CAP_BREED >= 0 && entity.isAdult) {
-      entity.age = Math.min(entity.age, plugin.config.AGE_CAP_BREED)
-    }
-
-    if (plugin.config.DEBUG) {
-      plugin.logger.info("Age of " + getMobDescription(entity) + " capped to " + entity.age)
-    }
-  }
-
-  fun limitCreatureSpawn(event: CreatureSpawnEvent) {
-    val reason = event.spawnReason
-
-    if ((reason == CreatureSpawnEvent.SpawnReason.BREEDING || reason == CreatureSpawnEvent.SpawnReason.EGG || reason == CreatureSpawnEvent.SpawnReason.DISPENSE_EGG) && isFarmAnimal(event.entity)) {
-
-      applyAgeCap(event.entity as Animals)
-
-      for (entity in event.entity.getNearbyEntities(4.0, 4.0, 4.0)) {
-        if (isFarmAnimal(entity)) {
-          applyAgeCap(entity as Animals)
+        if (!shouldLimitNaturalSpawn && !shouldLimitSpawnerSpawn) {
+            return
         }
-      }
 
-      return
-    }
+        if (!plugin.config.SPAWN_LIMITED_ENTITY_TYPES.contains(event.entityType) || !hasCap(event.entity)) {
+            return
+        }
 
-    val shouldLimitNaturalSpawn = plugin.config.LIMIT_NATURAL_SPAWN && (event.spawnReason == CreatureSpawnEvent.SpawnReason.BREEDING || event.spawnReason == CreatureSpawnEvent.SpawnReason.NATURAL || event.spawnReason == CreatureSpawnEvent.SpawnReason.DEFAULT)
-    val shouldLimitSpawnerSpawn = plugin.config.LIMIT_SPAWNER_SPAWN && event.spawnReason == CreatureSpawnEvent.SpawnReason.SPAWNER
+        val cap = getCap((getCapKey(event.entity)))
+        var count = 0
 
-    if (!shouldLimitNaturalSpawn && !shouldLimitSpawnerSpawn) {
-      return
-    }
+        for (otherEntity in event.location.chunk.entities) {
+            if (otherEntity.type == event.entityType) {
+                count++
+                if (count >= cap) {
+                    break
+                }
+            }
+        }
 
-    if (!plugin.config.SPAWN_LIMITED_ENTITY_TYPES.contains(event.entityType) || !hasCap(event.entity)) {
-      return
-    }
-
-    val cap = getCap((getCapKey(event.entity)))
-    var count = 0
-
-    for (otherEntity in event.location.chunk.entities) {
-      if (otherEntity.type == event.entityType) {
-        count++
         if (count >= cap) {
-          break
+            if (plugin.config.DEBUG) {
+                plugin.logger.info("Cancel spawn of " + getMobDescription((event.entity)) + " (reason = " + event.spawnReason.name.toLowerCase() + ", cap = " + cap + ")")
+            }
+            event.entity.remove()
         }
-      }
     }
 
-    if (count >= cap) {
-      if (plugin.config.DEBUG) {
-        plugin.logger.info("Cancel spawn of " + getMobDescription((event.entity)) + " (reason = " + event.spawnReason.name.toLowerCase() + ", cap = " + cap + ")")
-      }
-      event.entity.remove()
+    private fun isFarmAnimal(entity: Entity): Boolean {
+        return (entity is Animals) && entity !is Tameable
     }
-  }
-
-  private fun isFarmAnimal(entity: Entity): Boolean {
-    return (entity is Animals) && entity !is Tameable
-  }
 }
